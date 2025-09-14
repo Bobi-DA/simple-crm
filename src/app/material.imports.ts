79f1240ef3125b4e736211b9df6b8fe9cb165d34
Bobi-DA <masterjb81@gmail.com>
2025-09-14 20:36:23 +0200

    dialog complet

--- src/app/material.imports.ts
+++ src/app/material.imports.ts
@@ -5,8 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 // Hier kannst du beliebig weitere Material-Module hinzufügen
 export const MATERIAL_IMPORTS = [
@@ -17,5 +20,8 @@ export const MATERIAL_IMPORTS = [
   MatSidenavModule,
   MatTooltipModule,
   MatDialogModule,
-  MatInputModule
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule
 ];
