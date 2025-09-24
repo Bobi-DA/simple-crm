d52deb246dd9aa7a9e07fefeed22775d65e19b19
Bobi-DA <masterjb81@gmail.com>
2025-09-24 22:28:44 +0200

    Nutzer in Tabelle anzeigen

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,17 +2,38 @@ import { Component } from '@angular/core';
 import { MATERIAL_IMPORTS } from '../material.imports';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MATERIAL_IMPORTS],
+  imports: [MATERIAL_IMPORTS, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  users$!: Observable<any[]>;
+  allUsers:any = [];
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private firestore: Firestore) {
+  }
+
+  ngOnInit() {
+    const usersCollection = collection(this.firestore, 'users');
+    const users$ = collectionData(usersCollection, { idField: 'id' });
+
+    console.log('Users$: ', users$);
+
+    // Methode: subscribe()
+    users$.subscribe((users) => {
+      console.log('Alle Users aus Firestore: ', users);
+      this.allUsers = users;
+      // Hier kannst du mit den Daten arbeiten
+      // Beispiel: users.forEach(u => console.log(u.name));
+    });
   }
 
   openDialog() {
